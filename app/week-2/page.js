7cd862b8a2f61f22ca930a2aef9e4fd78443a7a8
michaelle7 <michael.le01@edu.sait.ca>
2024-09-24 18:16:56 -0600

    Did week 2 assignment

---
+++ app/week-2/page.js
@@ -0,0 +1,11 @@
+// week-2/page.js
+import StudentInfo from './student-info';
+
+export default function Week2Page() {
+  return (
+    <div>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </div>
+  );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,13 @@
+// student-info.js
+import Link from 'next/link';
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Michael Le</p>
+      <p> 
+        <Link href="https://github.com/michaelle7">https://github.com/michaelle7</Link>
+      </p>
+    </div>
+  );
+}
